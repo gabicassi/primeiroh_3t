49a51f22bb8f30911849b17f3b8014c4928bbf41
gabicassi <110493850+gabicassi@users.noreply.github.com>
2022-11-09 15:01:24 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,82 +1,34 @@
-//alert('Oi, eu sou o alert!')
-//console.log('Oi, eu estou no console');
-//document.write('aula de PC');
+"use strict";
 
-var tabuada = 8;
+let button = document.getElementById('calcular');
+let button_limpar = document.getElementById('limpar');
 
-function meChame(nome){
-    document.write("<h1>Tabuada do " + tabuada + "</h1>");
-    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br");
-    document.write("Feita por " + nome);
-    
-}
+button.addEventListener('click',calculo);
+button_limpar.addEventListener('click',limpar);
 
-function escreva(){
-    for(var i = 1; i < 11; i++){
-        document.write(tabuada + " x " + i + " = "+(tabuada*i)+"<br>");
+function calculo(e){
+    e.preventDefault();//para impedir que a página HTML sofra refresh
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let num3 = parseFloat(document.getElementById('num3').value);
+    let delta = Math.pow(num2,2) - 4*num1*num3;
+    alert(`O delta é: ${delta}`);
+    if(delta > 0){
+        let raiz1 = (-num2 + Math.sqrt(delta) )/(2*num1);
+        let raiz2 = (-num2 - Math.sqrt(delta) )/(2*num1);
+        document.getElementById("resultado").innerHTML = "Raiz 1 = " + raiz1 + "<br>" + "Raiz 2 = " + raiz2; 
+        
+    }else if (delta===0){
+            let raizUnica = (-num2 + Math.sqrt(delta) )/(2*num1);
+            document.getElementById("resultado").innerHTML = "Raiz única = " + raizUnica;
+    }else if (delta < 0){
+        document.getElementById("resultado").innerHTML = "Delta < 0, não existem raízes no conjunto dos números reais.";
     }
 }
 
-function quadrado(){
-    for(var i = 2; i < 101; i++){
-        document.write("O Quadrado de " + i + " é " + (i*i)+ "<br>")
-    }
-}
-function total(){
-    let valor = document.getElementById("val").value;
-    let juros = document.getElementById("ju").value;
-    let mes = document.getElementById("mes").value;
-    let resultado = 0;
-   
-   if(!Number(valor)){
-    alert( "O juros deve ser um número.")   
-    return
-}
-if(!Number(mes)){
-    alert( "O mês deve ser um número.")   
-    return
-}
-if(!Number(juros)){
-    alert( "O valor deve ser um número.")   
-    return
-}
-
-
-   
-    for(let i = 1; i <= mes; i++){
-        resultado = valor * (1+(juros/100));
-        valor = resultado;
-    }
-    
-    document.write("O Resultado é " + resultado);
-}
-function soma(){
-    let n1 = document.getElementById("n1").value;
-    let n2 = document.getElementById("n2").value;
-    let n3 = document.getElementById("n3").value;
-    let r = Number(n1) + Number(n2) + Number(n3);
-    document.getElementById("resultado").innerHTML = r;
-}
-function média(){
-    let n1 = document.getElementById("n1").value;
-    let n2 = document.getElementById("n2").value;
-    let n3 = document.getElementById("n3").value;
-    let r = (Number(n1) + Number(n2) + Number(n3))/3;
-    document.getElementById("resultado").innerHTML = r;
-}
-function precisa(){
-    let n1 = document.getElementById("n1").value;
-    let n2 = document.getElementById("n2").value;
-    let n3 = document.getElementById("n3").value;
-    let r = 180 - (Number(n1) + Number(n2) + Number(n3));
-    document.getElementById("resultado").innerHTML = r;
+function limpar(e){
+    document.getElementById('num1').value = " ";
+    document.getElementById('num2').value = " ";
+    document.getElementById('num3').value = " ";
+    document.getElementById('resultado').innerHTML = null;
 }
